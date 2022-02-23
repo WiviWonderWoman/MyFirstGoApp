f37a468510a1c7ca4101f7a90322ea3f33429df1
Wivianne Grapenholt <w.grapenholt@gmail.com>
2022-02-23 17:09:56 +0100

    tidying up and commented

--- controllers/user.go
+++ controllers/user.go
@@ -9,10 +9,12 @@ type userController struct {
 	userIDPattern *regexp.Regexp
 }
 
+// ServHTTP is a METHOD - a function associeted with a data type (in this case a userController)
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the User Controller!"))
 }
 
+// newUserController is a CONSTRUCTOR function
 func newUserController() *userController {
 	return &userController{
 		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 )
 
 func main() {
-
+	// call function RegisterControllers
 	controllers.RegisterControllers()
+	// ListenAndServe listens on the TCP network address addr and then calls Serve with handler to handle requests on incoming connections.
+	//The handler is typically nil, in which case the DefaultServeMux is used.
+	// ListenAndServe always returns a non-nil error. //* func ListenAndServe(addr string, handler Handler) error
 	http.ListenAndServe(":3000", nil)
+
 	/* u := models.User{
 		ID:        2,
 		FirstName: "Tricia",
