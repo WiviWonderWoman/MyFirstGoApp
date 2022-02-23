43c1881980a36ce64c28e1547f3369a224d2d424
Wivianne Grapenholt <w.grapenholt@gmail.com>
2022-02-23 17:44:52 +0100

    for loop with condition

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	// "fmt"
-	"net/http"
-
-	"github.com/WiviWonderWoman/MyFirstGoApp/controllers"
+	"fmt"
+	// "net/http"
+	// "github.com/WiviWonderWoman/MyFirstGoApp/controllers"
 	// "github.com/WiviWonderWoman/My-first-Go-App/models"
 )
 
 func main() {
 	// call function RegisterControllers
-	controllers.RegisterControllers()
+	// controllers.RegisterControllers()
 	// ListenAndServe listens on the TCP network address addr and then calls Serve with handler to handle requests on incoming connections.
 	//The handler is typically nil, in which case the DefaultServeMux is used.
 	// ListenAndServe always returns a non-nil error. //* func ListenAndServe(addr string, handler Handler) error
-	http.ListenAndServe(":3000", nil)
+	// http.ListenAndServe(":3000", nil)
 
 	/* u := models.User{
 		ID:        2,
@@ -22,4 +21,22 @@ func main() {
 		LastName:  "McMillan",
 	}
 	fmt.Println(u) */
+	loopWithCondition(true)
+}
+
+func loopWithCondition(run bool) {
+	if run {
+
+		var i int
+		// loop untill i is less then 5
+		for i < 5 {
+			fmt.Println("loop- lap:", i) //* if break output will be: "loop- lap: 0 loop- lap: 1 loop- lap: 2"
+			i++                          // mutate i so we don't get an infinite loop
+			if i == 3 {
+				// break //* stop the enthire loop before conditions are met
+				continue // continue till next lap without printing the 'continuing...'
+			}
+			fmt.Println("continuing...") // outputs NO 'continuing...' between 2 and 3
+		}
+	}
 }
